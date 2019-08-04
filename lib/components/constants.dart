3e54f7b009865ed2bd12e845a689d5518563937f
Max Walters <max.walters10@gmail.com>
2019-08-04 16:34:02 -0400

    Set up constants

---
+++ lib/components/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const kCardTextColor = Color(0xFFBBDDAC);
+
+const kCardTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: kCardTextColor,
+);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: kCardTextColor,
+  fontWeight: FontWeight.w900,
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kActiveCardColor = Color(0xa0458926);
+const kInactiveCardColor = Color(0xa04d9a2a);
