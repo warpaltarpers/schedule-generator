f59fd9cc3466c7c19b78d409f182fce5cd9a8211
Max Walters <max.walters10@gmail.com>
2019-08-07 12:05:39 -0400

    Generator rebuilt

--- lib/generator_scroll.dart
+++ lib/generator_scroll.dart
@@ -151,12 +151,237 @@ class _GeneratorScrollState extends State<GeneratorScroll> {
                 cardColor: kActiveCardColor,
                 cardChild: Column(
                   children: <Widget>[
-                    Text(
-                      'JOB FOOTAGE',
-                      style: kLabelTextStyle,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: Text(
+                        'JOB FOOTAGE',
+                        style: kLabelTextStyle,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: <Widget>[
+                        Text(
+                          jobFoot.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Text(
+                          ' ft',
+                          style: kLabelTextStyle,
+                        ),
+                      ],
+                    ),
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        thumbColor: Colors.white,
+                        overlayColor: Color(0x29FFFFFF),
+                        thumbShape: RoundSliderThumbShape(
+                          enabledThumbRadius: 15.0,
+                        ),
+                        overlayShape: RoundSliderOverlayShape(
+                          overlayRadius: 30.0,
+                        ),
+                        activeTrackColor: Colors.white,
+                        inactiveTrackColor: Color(0xFFb9e682),
+                      ),
+                      child: Slider(
+                        value: jobFoot.toDouble(),
+                        min: 0.0,
+                        max: 1000.0,
+                        onChanged: (double newValue) {
+                          setState(() {
+                            jobFoot = newValue.round();
+                          });
+                        },
+                      ),
                     ),
                   ],
                 ),
+              ),
+              Row(
+                children: <Widget>[
+                  // Filter size dropdown
+                  Expanded(
+                    child: ReusableCard(
+                      cardColor: kActiveCardColor,
+                      cardChild: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text('SIZE', style: kLabelTextStyle,),
+                              SizedBox(height: 5.0),
+                              DropdownButton<String>(
+                                items: sizes,
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    filterSize = newValue;
+                                  });
+                                },
+                                value: filterSize,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    // Filter color dropdown
+                    child: ReusableCard(
+                      cardColor: kActiveCardColor,
+                      cardChild: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text('COLOR', style: kLabelTextStyle,),
+                              SizedBox(height: 5.0),
+                              DropdownButton<String>(
+                                items: colors,
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    filterColor = newValue;
+                                  });
+                                },
+                                value: filterColor,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              ReusableCard(
+                cardColor: kActiveCardColor,
+                cardChild: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: Text(
+                        '# OF MITERS',
+                        style: kLabelTextStyle,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: <Widget>[
+                        Text(
+                          numMiters.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Text(
+                          ' miters',
+                          style: kLabelTextStyle,
+                        ),
+                      ],
+                    ),
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        thumbColor: Colors.white,
+                        overlayColor: Color(0x29FFFFFF),
+                        thumbShape: RoundSliderThumbShape(
+                          enabledThumbRadius: 15.0,
+                        ),
+                        overlayShape: RoundSliderOverlayShape(
+                          overlayRadius: 30.0,
+                        ),
+                        activeTrackColor: Colors.white,
+                        inactiveTrackColor: Color(0xFFb9e682),
+                      ),
+                      child: Slider(
+                        value: numMiters.toDouble(),
+                        min: 0.0,
+                        max: 100.0,
+                        onChanged: (double newValue) {
+                          setState(() {
+                            numMiters = newValue.round();
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(
+                      cardColor: kActiveCardColor,
+                      cardChild: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text('STORIES', style: kLabelTextStyle,),
+                              SizedBox(height: 5.0),
+                              DropdownButton<String>(
+                                items: stories,
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    storyCount = newValue;
+                                  });
+                                },
+                                value: storyCount,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      cardColor: kActiveCardColor,
+                      cardChild: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text('PAYMENT', style: kLabelTextStyle,),
+                              SizedBox(height: 5.0),
+                              DropdownButton<String>(
+                                items: payments,
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    paymentType = newValue;
+                                  });
+                                },
+                                value: paymentType,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              BottomButton(
+                buttonTitle: 'GENERATE',
+                onTap: (){
+                  if(jobNumCard.returnInput() != null && cityCard != null) {
+                    Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => BlockPage(
+                      jobNum: jobNumCard.returnInput(),
+                      location: cityCard.returnInput(),
+                      jobFootage: jobFoot,
+                      filterSize: filterSize,
+                      filterColor: filterColor,
+                      miters: numMiters,
+                      stories: storyCount,
+                      payment: paymentType,
+                    )));
+                  } else {
+                    return;
+                  }
+                },
               ),
             ],
           ),
